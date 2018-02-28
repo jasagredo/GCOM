5493295328028de7b143fccc8e4c2f5fd8e45e57
Javier Sagredo <jasataco@gmail.com>
2018-02-28 15:35:50 +0100

    Iniciado el dibujado de rectas. Es posible que no haya un centro para K>=4?

--- PR1/dibujapuntos.py
+++ PR1/dibujapuntos.py
@@ -25,6 +25,8 @@ class CreatePoints(object):
         self.clase_max = 0
         self.clase_actual = 0
 
+        self.rectas_a_borrar = 0
+
         self.metodo = None
         self.x0 = None
         self.y0 = None
@@ -85,7 +87,7 @@ class CreatePoints(object):
                 print("No has introducido nada para la clase mas alta")
             return
 
-        if event.inaxes == self.axb4:
+        elif event.inaxes == self.axb4:
             if self.clase_actual > 0:
                 self.clase_actual -= 1
                 self.ax.set_title('Introduciendo puntos para la clase {0}'.format(self.clase_actual))
@@ -94,144 +96,73 @@ class CreatePoints(object):
                 print("No existen clases negativas")
             return
 
-        if event.inaxes == self.axb1:
-            X = np.array(self.x).T
-            self.metodo = LeastSquares()
-            print(self.metodo.train(X, self.t))
-            # Seria interesante exportar esto a un metodo suelto
-            # Generar matriz de puntos, llamar a classify de self.metodo y pintarlo
-            if self.clase_max == 1:
-                wt = self.metodo.w_tilde.T
-                rec = wt[0] - wt[1]
-                x = np.arange(-20, 20)
-                y = (rec[1]*x - rec[0])/-rec[2]
+        elif event.inaxes == self.axb1:
+            if self.clase_max > 0:
+                if self.rectas_a_borrar > 0:
+                    self.borrar_rectas()
+                X = np.array(self.x).T
+                self.metodo = LeastSquares()
+                print(self.metodo.train(X, self.t))
+                if self.clase_max == 1:
+                    wt = self.metodo.w_tilde.T
+                    rec = wt[0] - wt[1]
+                    x = np.arange(-20, 20)
+                    y = (rec[1]*x - rec[0])/-rec[2]
+                    self.ax.plot(x, y)
+                    self.rectas_a_borrar += 1
+                    self.fig.canvas.draw()
+                elif self.clase_max >= 2:
+                    wt = self.metodo.w_tilde.T
+                    rec1 = wt[0] - wt[1]
+                    rec2 = wt[0] - wt[2]
+                    sis = np.vstack([rec1, rec2])
+                    centro = np.linalg.solve(sis[:, 1:], sis[:, 0])
+                    for i in range(0, self.clase_max+1):
+                        for j in range(i+1, self.clase_max+1):
+                            rec = wt[i] - wt[j]
+                            self.draw_line(rec, centro, (i, j))
+                    self.fig.canvas.draw()
 
-                self.ax.plot(x, y)
-                self.fig.canvas.draw()
-            elif self.clase_max == 2:
-                wt = self.metodo.w_tilde.T
-                rec1 = wt[0] - wt[1]
-                rec2 = wt[0] - wt[2]
-                rec3 = wt[1] - wt[2]
-                sis = np.vstack([rec1, rec2])
-                centro = np.linalg.solve(sis[:, 1:], sis[:, 0])
-                xizq = np.arange(-20, centro[0], 0.25)
-                xder = np.arange(centro[0], 20, 0.25)
-                c1 = self.metodo.classify(np.array([20, (rec1[1] * 20 - rec1[0]) / -rec1[2]]))
-                c2 = self.metodo.classify(np.array([20, (rec2[1] * 20 - rec2[0]) / -rec2[2]]))
-                c3 = self.metodo.classify(np.array([20, (rec3[1] * 20 - rec3[0]) / -rec3[2]]))
-                if  c1 == 0 or c1 == 1:
-                    x1 = xder
-                else:
-                    x1 = xizq
-
-                if c2 == 0 or c2 == 2:
-                    x2 = xder
-                else:
-                    x2 = xizq
-
-                if c3 == 1 or c3 == 2:
-                    x3 = xder
-                else:
-                    x3 = xizq
-                y1 = (rec1[1] * x1 - rec1[0]) / -rec1[2]
-                y2 = (rec2[1] * x2 - rec2[0]) / -rec2[2]
-                y3 = (rec3[1] * x3 - rec3[0]) / -rec3[2]
-                self.ax.plot(x1, y1, x2, y2, x3, y3)
-                self.fig.canvas.draw()
-            elif self.clase_max == 3:
-                wt = self.metodo.w_tilde.T
-                rec1 = wt[0] - wt[1]
-                rec2 = wt[0] - wt[2]
-                rec3 = wt[0] - wt[3]
-                rec4 = wt[1] - wt[2]
-                rec5 = wt[1] - wt[3]
-                rec6 = wt[2] - wt[3]
-                sis = np.vstack([rec1, rec2])
-                centro = np.linalg.solve(sis[:, 1:], sis[:, 0])  # Aparentemente no tiene por que existir un unico centro con 4 clases.
-                xizq = np.arange(-20, centro[0], 0.25)
-                xder = np.arange(centro[0], 20, 0.25)
-                c1 = self.metodo.classify(np.array([20, (rec1[1] * 20 - rec1[0]) / -rec1[2]]))
-                c2 = self.metodo.classify(np.array([20, (rec2[1] * 20 - rec2[0]) / -rec2[2]]))
-                c3 = self.metodo.classify(np.array([20, (rec3[1] * 20 - rec3[0]) / -rec3[2]]))
-                c4 = self.metodo.classify(np.array([20, (rec4[1] * 20 - rec4[0]) / -rec4[2]]))
-                c5 = self.metodo.classify(np.array([20, (rec5[1] * 20 - rec5[0]) / -rec5[2]]))
-                c6 = self.metodo.classify(np.array([20, (rec6[1] * 20 - rec6[0]) / -rec6[2]]))
-                if c1 == 0 or c1 == 1:
-                    x1 = xder
-                else:
-                    x1 = xizq
-
-                if c2 == 0 or c2 == 2:
-                    x2 = xder
-                else:
-                    x2 = xizq
-
-                if c3 == 0 or c3 == 3:
-                    x3 = xder
-                else:
-                    x3 = xizq
-
-                if c4 == 1 or c4 == 2:
-                    x4 = xder
-                else:
-                    x4 = xizq
-
-                if c5 == 1 or c5 == 3:
-                    x5 = xder
-                else:
-                    x5 = xizq
-
-                if c6 == 2 or c6 == 3:
-                    x6 = xder
-                else:
-                    x6 = xizq
-                y1 = (rec1[1] * x1 - rec1[0]) / -rec1[2]
-                y2 = (rec2[1] * x2 - rec2[0]) / -rec2[2]
-                y3 = (rec3[1] * x3 - rec3[0]) / -rec3[2]
-                y4 = (rec4[1] * x4 - rec4[0]) / -rec4[2]
-                y5 = (rec5[1] * x5 - rec5[0]) / -rec5[2]
-                y6 = (rec6[1] * x6 - rec6[0]) / -rec6[2]
-
-                self.ax.plot(x1, y1, x2, y2, x3, y3, x4, y4, x5, y5, x6, y6)
-                self.fig.canvas.draw()
-            clase_fondo = self.metodo.classify(self.fondo)
-            print(clase_fondo)
+                clase_fondo = self.metodo.classify(self.fondo)
+                print(clase_fondo)
+            else:
+                print("Como vas a clasificar si solo tienes una clase, Sherlock")
             return
 
-        if event.inaxes == self.axb2:
+        elif event.inaxes == self.axb2:
             X = np.array(self.x).T
             self.metodo = LDA()
             print(self.metodo.train(X, self.t))
             #por ahora no podemos clasificar
             return
 
-        x0, y0 = event.xdata, event.ydata
-        for circle in self.circle_list:
-            contains, attr = circle.contains(event)
-            if contains:
-                self.press_event = event
-                self.current_circle = circle
-                self.x0, self.y0 = self.current_circle.center
-                self.current_x = self.x.index(self.current_circle.center)
-                return
-
-        c = Circle((x0, y0), 0.5, color='C{0}'.format(self.clase_actual))
-        self.ax.add_patch(c)
-        self.circle_list.append(c)
-        self.x.append((x0, y0))
-
-        if self.clase_actual == 0 and self.clase_max == 0:
-            self.t = np.hstack(([self.t, 1]))
         else:
-            vec_can = np.zeros(self.clase_max + 1)
-            vec_can[self.clase_actual] = 1
-            self.t = np.hstack([self.t, vec_can.reshape(vec_can.shape[0], 1)])
-        if self.clase_actual == self.clase_max:
-            self.conteo_clase_max += 1
+            x0, y0 = event.xdata, event.ydata
+            for circle in self.circle_list:
+                contains, attr = circle.contains(event)
+                if contains:
+                    self.press_event = event
+                    self.current_circle = circle
+                    self.x0, self.y0 = self.current_circle.center
+                    self.current_x = self.x.index(self.current_circle.center)
+                    return
 
-        self.current_circle = None
-        self.fig.canvas.draw()
+            c = Circle((x0, y0), 0.5, color='C{0}'.format(self.clase_actual))
+            self.ax.add_patch(c)
+            self.circle_list.append(c)
+            self.x.append((x0, y0))
+
+            if self.clase_actual == 0 and self.clase_max == 0:
+                self.t = np.hstack(([self.t, 1]))
+            else:
+                vec_can = np.zeros(self.clase_max + 1)
+                vec_can[self.clase_actual] = 1
+                self.t = np.hstack([self.t, vec_can.reshape(vec_can.shape[0], 1)])
+            if self.clase_actual == self.clase_max:
+                self.conteo_clase_max += 1
+
+            self.current_circle = None
+            self.fig.canvas.draw()
 
     def on_release(self, event):
         if self.current_circle is not None:
@@ -257,6 +188,20 @@ class CreatePoints(object):
         self.current_circle.center = self.x0 + dx, self.y0 + dy
         self.fig.canvas.draw()
 
+    def draw_line(self, eqn, cnt, clases):
+        clase = self.metodo.classify(np.array([20, (eqn[1] * 20 - eqn[0]) / -eqn[2]]))
+        if clase == clases[0] or clase == clases[1]:
+            x = np.arange(cnt[0], 20, 0.25)
+        else:
+            x = np.arange(-20, cnt[0], 0.25)
+        rec = (eqn[1] * x - eqn[0]) / -eqn[2]
+        self.rectas_a_borrar += 1
+        self.ax.plot(x, rec)
+
+    def borrar_rectas(self):
+        for i in range(self.rectas_a_borrar):
+            self.ax.lines.pop(0)
+        self.rectas_a_borrar = 0
 
 if __name__ == '__main__':
 
