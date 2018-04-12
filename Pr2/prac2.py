fdd3f886d58e236f38c940f2adb155d961e9a96d
Javier Sagredo <jasataco@gmail.com>
2018-04-12 13:39:09 +0200

    Conseguida una posible clasificación
    Se consigue un 17% aprox de error, que no está tan mal. Ahora realiza 3 pases por la base de training. Además se realiza un nuevo shuffle antes de hacer el training. Se ha añadido un parámetro i al constructor del perceptrón por si fuese necesario para saber el dígito que identifica ese perceptrón.

--- Pr2/prac2.py
+++ Pr2/prac2.py
@@ -1,3 +1,4 @@
+from __future__ import division, print_function
 import numpy as np
 from sklearn.datasets import fetch_mldata
 from Perceptron import *
@@ -16,12 +17,29 @@ test_x = test[:, :test.shape[1]-1]
 test_t = test[:, test.shape[1]-1:]
 perceptrones = []
 for i in range(10):
-    per = Perceptron(data.shape[1])
+    per = Perceptron(data.shape[1], i)
     aux = np.equal(train_t, np.ones_like(train_t)*i).reshape(train_x.shape[0])
     X_1 = train_x[aux, :]
     X_2 = train_x[np.logical_not(aux), :]
     X = np.vstack([X_1, X_2])
     T = np.hstack([np.ones(X_1.shape[0]), np.ones(X_2.shape[0])*(-1)]).T
-    print 'Comienza train de perceptron {}'.format(i)
+    aux2 = np.hstack([X, T.reshape(T.shape[0],1)])
+    np.random.shuffle(aux2)
+    X = aux2[:, :aux2.shape[1] - 1]
+    T = aux2[:, aux2.shape[1] - 1:]
+    print('Comienza train de perceptron {0}'.format(i))
     per.train(X, T)
     perceptrones.append(per)
+mal_c = 0
+cl = 0
+print('Comienza el test')
+for i in range(test_x.shape[0]):
+    arr = []
+    for per in perceptrones:
+        arr.append(per.eval(test_x[i]))
+    sal = np.argmax(np.array(arr))
+    cl = cl + 1
+    print('{0}% completado'.format(i*100/test_x.shape[0]))
+    if test_t[i] != sal:
+        mal_c = mal_c + 1
+print('Porcentaje de fallo de clasificacion en el test {0}'.format(mal_c*100/cl))
